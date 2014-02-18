66746b3e146f1a175bacd5adab86ec6890792213
diegoceccarelli <diego.ceccarelli@gmail.com>
2014-02-18 22:32:12 +0100

    add logback rules

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,7 +6,7 @@ LOG=INFO
 ##LOG=DEBUG
 LOGAT=1000
 E_BADARGS=65
-JAVA="java $XMX -Dlogat=$LOGAT -Dlog=$LOG -cp ./target/dexter-eval-0.0.1-SNAPSHOT-jar-with-dependencies.jar "
+JAVA="java $XMX -Dlogat=$LOGAT -Dlog=$LOG -cp .:./target/dexter-eval-0.0.1-SNAPSHOT-jar-with-dependencies.jar "
 CLI=it.cnr.isti.hpc.dexter.eval.cli
 
 export LC_ALL=C
