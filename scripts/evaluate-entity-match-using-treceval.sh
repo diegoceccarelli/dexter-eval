2accd86976a31204328b22815fcb338322182ad0
diegoceccarelli <diego.ceccarelli@gmail.com>
2014-02-18 22:33:53 +0100

    improve treveval output

--- scripts/evaluate-entity-match-using-treceval.sh
+++ scripts/evaluate-entity-match-using-treceval.sh
@@ -11,10 +11,13 @@ fi
 
 PREDICTIONS=/tmp/prediction.treceval
 QRELS=/tmp/gt.qrels
+TREC_EVAL=./bin/trec_eval8.1 
 
 echo "convert predictions $1 to treceval format in $PREDICTIONS"
 $JAVA $CLI.TracevalConverterCLI  -input $1 -output $PREDICTIONS
 echo "convert goldentruth $2 to treceval format in $QRELS"
 $JAVA $CLI.QrelConverterCLI  -input $2 -output $QRELS
+$TREC_EVAL -a $QRELS $PREDICTIONS
+./bin/trec_eval -m set_P -m set_recall  $QRELS $PREDICTIONS
 
 
