005339151962fbdbd48f4e54a9d9a61f915147bf
diegoceccarelli <diego.ceccarelli@gmail.com>
2014-02-22 18:54:19 +0100

    add metrics as parameter

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 source ./scripts/config.sh
 
-EXPECTED_ARGS=3 
+EXPECTED_ARGS=4 
 
 METRICS=metrics.txt
+DEBUG=False
+#DEBUG=True
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-  echo "Usage: `basename $0` predictions[.json,.tsv] goldentruth[.json,.tsv] comparator"
+  echo "Usage: `basename $0` predictions[.json,.tsv] goldentruth[.json,.tsv] comparator metrics.txt"
   exit $E_BADARGS
 fi
 
 echo "evaluate prediction $1 against $2, using comparator $3"
-$JAVA -Dmetrics=metrics.txt $CLI.EvaluatorCLI -input $1 -gt $2 -cmp $3
+$JAVA -Ddebug=$DEBUG -Dmetrics=$4 $CLI.EvaluatorCLI -input $1 -gt $2 -cmp $3
